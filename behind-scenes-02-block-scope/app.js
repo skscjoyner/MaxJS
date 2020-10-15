1c55d7e65bb13239d395a86995bd4a35af9d7928
skscjoyner <backtothen@gmail.com>
2020-10-15 17:00:25 -0400

    added notes

--- behind-scenes-02-block-scope/app.js
+++ behind-scenes-02-block-scope/app.js
@@ -1,15 +1,19 @@
-let name = 'Max';
+// NOW - use 'let' and 'const' only. 
+// 'var' is global scoped and very flexible.
+
+var name = 'Max';
 
 if (name === 'Max') {
-  let hobbies = ['Sports', 'Cooking'];
+  var hobbies = ['Sports', 'Cooking'];
   console.log(hobbies);
-
 }
 
+// Using 'var' keyword will allow variable to be global scope and will not throw an error.
+// 'let' and 'const' are block scope variables and more restrictive; thus better.
 function greet() {
-  let age = 30;
-  let name = 'Manuel';
-  console.log(name, age, hobbies);
+  var age = 30;
+  var name = 'Manuel';
+  console.log(name, age, hobbies); // hobbies is not defined inside the function and will not display
 }
 
 console.log(name, hobbies);
