8e5484b667a3572456e231147f1fe644cb109074
skscjoyner <backtothen@gmail.com>
2020-10-26 16:42:59 -0400

    initial commit

---
+++ dom-assignment-start/assignment.js
@@ -0,0 +1,21 @@
+// Question 1
+
+const li = document.querySelector('#task-1');
+const li2 = document.getElementById('task-1');
+
+li.style.color = 'white';
+li2.style.backgroundColor = 'black';
+
+// Question 2
+
+// const title = document.querySelector('title');
+// title.textContent = 'Assignment - Solved';
+
+const head = document.head;
+const title = document.head.querySelector('title');
+title.textContent = 'Assignment - Solved!'
+
+// Question 3
+
+const h1 = document.querySelector('h1');
+h1.textContent = 'Assignment - Solved!';
