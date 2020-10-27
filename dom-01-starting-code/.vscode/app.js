9743c5c59bdce0b2fad1c7638f9dbe3d263128b6
skscjoyner <backtothen@gmail.com>
2020-10-27 12:25:13 -0400

    initial commit

--- dom-01-starting-code/.vscode/app.js
+++ dom-01-starting-code/.vscode/app.js
@@ -15,3 +15,41 @@ const listItemElements = document.getElementsByTagName('li'); // gives live list
 for (const listItemEl of listItemElements) {
   console.dir(listItemEl);
 }
+
+// */*/* SELECTOR NOTES *\*\*
+
+document.querySelector('.list-item') // selects first list-item match
+const ul = document.querySelector('ul'); 
+ul.children[1] // selects 2nd item -- NO TEXT NODES - COLLECTION OF element NODES
+ul.childNodes // selects all child nodes...includes TEXT NODES - Can add CSS (white-space: pre) to display TEXT NODES
+ul.firstChild // first TEXT or ELEMENT CHILD NODE
+ul.firstElementChild // first ELEMENT CHILD NODE
+ul.lastChild // last TEXT or ELEMENT CHILD NODE
+ul.lastElementChild // last ELEMENT CHILD NODE
+
+// PARENT / ANCESTOR - NOT MANY USE CASES
+
+const liFirst = document.querySelector('li');
+liFirst.parentNode // nearest parent node NOT TEXT NODEs 
+liFirst.parentElement 
+
+liFirst.closest('body'); // to select body ANCESTOR ELEMENT
+
+
+// SELECT SIBLING ELEMENTS
+
+const ul = li.parentElement; // to get to other element nodes.
+ul.previousSibling // 'text' 
+ul.previousElementSibling // 'HEADER
+ul.nextSibling // TEXT
+ul.nextElementSibling // input
+
+
+// */*/* DOM Traversal *\*\*
+
+const ul = document.body.firstElementChild.nextElementSibling;
+const firstLi = ul.firstElementChild;
+console.log(firstLi); // 'li'
+
+
+
