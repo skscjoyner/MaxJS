6d0d4667c475520f31783c88799ab2c4bd4c0637
skscjoyner <backtothen@gmail.com>
2020-11-09 11:42:15 -0500

    additional notes

--- dom-04-styling/app.js
+++ dom-04-styling/app.js
@@ -19,3 +19,20 @@ button.addEventListener('click', () => {
   // section.classList.toggle('visible');
   section.classList.toggle('invisible');
 });
+
+// will not lose original content and WILL NOT allow direct access.
+div.insertAdjacentHTML('beforebegin, afterbegin, beforeend, afterend', 'SOME TEXT');
+
+// document.createElement('TAG-NAME (ex. 'li'))
+
+// */*/* ADDITIONAL METHODS *\*\*
+// INSERTING AN ELEMENT MOVES IT. CLONE IS BETTER.
+
+// append
+// appendChild
+// prepend
+// before
+// after
+// replaceWith
+// insertAdjacentElement // saffer and has better browser support
+// const newLi2 = newLi.cloneNode(true) // clones or duplicates the node.
