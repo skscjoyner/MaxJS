2dd2b0a1228b76f78871b1e175e0b261ced430c5
skscjoyner <backtothen@gmail.com>
2020-11-11 14:19:41 -0500

    add, display, delete movie

--- dom-06-demo-project-setup/assets/scripts/app.js
+++ dom-06-demo-project-setup/assets/scripts/app.js
@@ -7,7 +7,6 @@ const modalBackdrop = document.getElementById("backdrop");
 // const modalBackdrop = document.body.firstElementChild;
 const cancelAddMovieButton = addMovieModal.querySelector(".btn--passive");
 const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
-
 const userInputs = addMovieModal.querySelectorAll('input');
 // const userInputs = addMovieModal.getElementsByTagName('input');
 const entryTextSection = document.getElementById('entry-text');
@@ -22,7 +21,21 @@ const updateUI = () => {
   }
 };
 
-const renderNewMovieElement = (title, imageUrl, rating) => {
+const deleteMovieHandler = (movieId) => {
+  let movieIndex = 0;
+  for (const movie of movies) {
+    if (movie.id === movieId) {
+      break;
+    }
+    movieIndex++;
+  }
+  movies.splice(movieIndex, 1);
+  const listRoot = document.getElementById('movie-list');
+  listRoot.children[movieIndex].remove();
+  // listRoot.removeChild(listRoot.children[movieIndex]);
+};
+
+const renderNewMovieElement = (id, title, imageUrl, rating) => {
   const newMovieElement = document.createElement('li');
   newMovieElement.className = 'movie-element';
   newMovieElement.innerHTML = `
@@ -34,18 +47,18 @@ const renderNewMovieElement = (title, imageUrl, rating) => {
       <p>${rating}/5 stars</p>
     </div>
     `;
+    newMovieElement.addEventListener('click', deleteMovieHandler.bind(null, id));
     const listRoot = document.getElementById('movie-list');
     listRoot.append(newMovieElement);
 };
 
 const toggleBackdrop = () => {
-  modalBackdrop.classList.toggle("visible");
+  modalBackdrop.classList.toggle('visible');
 };
 
-const toggleMovieModal = () => {
-  // function() {} (can use either method to declare a function.)
+const toggleMovieModal = () => { // function() {} (can use either method to declare a function.)
   // addMovieModal.className = 'class name'; // problem keeping track of names. LESS CLEAR
-  addMovieModal.classList.toggle("visible");
+  addMovieModal.classList.toggle('visible');
   toggleBackdrop();
 };
 
@@ -64,9 +77,9 @@ const addMovieHandler = () => {
   const titleValue = userInputs[0].value;
   const imageUrlValue = userInputs[1].value;
   const ratingValue = userInputs[2].value;
-
+  // validation check for inputs
   if (
-    titleValue.trim() === "" ||
+    titleValue.trim() === "" || // .trim() removes the extra white space -- not between two words.
     imageUrlValue.trim() === "" ||
     ratingValue.trim() === "" ||
     +ratingValue < 1 ||
@@ -77,6 +90,7 @@ const addMovieHandler = () => {
   }
 
   const newMovie = {
+    id: Math.random().toString(),
     title: titleValue,
     image: imageUrlValue,
     rating: ratingValue
@@ -86,7 +100,7 @@ const addMovieHandler = () => {
   console.log(movies);
   toggleMovieModal();
   clearMovieInputs();
-  renderNewMovieElement(newMovie.title, newMovie.image, newMovie.rating);
+  renderNewMovieElement(newMovie.id, newMovie.title, newMovie.image, newMovie.rating);
   updateUI();
 };
 
