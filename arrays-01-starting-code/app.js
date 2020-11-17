40d0a3d8c4815bc6937fd9ccae76260d32d6b41e
skscjoyner <backtothen@gmail.com>
2020-11-17 09:45:31 -0500

    more notes

--- arrays-01-starting-code/app.js
+++ arrays-01-starting-code/app.js
@@ -1,15 +1,21 @@
 const numbers = [1, 2, 3]; // preferred way to create an array
 console.log('numbers -', numbers);
 
-// // const moreNumbers = new Array(); // equals [] - length of 0.
-// // const moreNumbers = new Array('Hi!', 'Bye'); // ['Hi!', 'Bye']
-// const moreNumbers = new Array(5); // STRANGE BEHAVIOR - 5 'empty' fixed-length array of # length
+// // WILL LIKELY NOT USE THE FOLLOWING METHOD.
+// // const moreNumbers = new Array(); // equals [] - length of 0. Constructor function.
+
+// const moreNumbers = new Array('Hi!', 'Welcome'); // ['Hi!', 'Welcome']
+// console.log('more numbers -', moreNumbers);
+
+// const moreNumbers = new Array(5); // STRANGE BEHAVIOR - 5 'empty' fixed-length array of '#' length
 // // can omit 'new' keyword and will get the same respone.
 // console.log('more numbers -', moreNumbers);
 
 // const yetMoreNumbers = Array.of(1, 2); // slower than using [].
 // console.log('yetMoreNumbers - ', yetMoreNumbers);
 
+
+// // CHECK THIS OUT
 // const moreNumbers = Array.from("Hi!"); // will make an array out of what's in the ().
 // console.log('moreNumbers -', moreNumbers);
 
