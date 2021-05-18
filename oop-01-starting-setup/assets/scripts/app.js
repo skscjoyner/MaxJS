31a8364cdddeb70b34d06e38a2d17ae2800b563d
skscjoyner <backtothen@gmail.com>
2021-05-18 11:46:53 -0400

    adding and using multiple classes

--- oop-01-starting-setup/assets/scripts/app.js
+++ oop-01-starting-setup/assets/scripts/app.js
@@ -1,3 +1,4 @@
+// THIS CLASS IS THE BLUEPRINT FOR THE ITEM - WHAT IT LOOKS LIKE
 class Product {
   // */*/* THESE 'FIELDS' ARE NOT NEEDED SINCE THEY ARE ASSIGNED IN THE CONSTRUCTOR  *\*\*
   // title = 'DEFAULT';
@@ -13,8 +14,33 @@ class Product {
   }
 }
 
-const productList = {
-  products: [
+// THIS CLASS CONTAINS THE LOGIC TO CREATE AND RENDER EACH ITEM
+class ProductItem {
+  constructor(product) {
+    this.product = product;
+  }
+
+  render() { // responsible for rendering a single item
+    const prodEl = document.createElement('li'); // Create an 'li' element 
+    prodEl.className = "product-item"; // for styling from CSS
+    prodEl.innerHTML = `
+      <div>
+        <img src="${this.product.imageUrl}" alt="${this.product.title}" >
+        <div class="product-item__content"> 
+          <h2>${this.product.title}</h2>
+          <h3>\$${this.product.price}</h3>
+          <p>${this.product.description}</p>
+          <button>Add To Cart</button>
+        </div>
+      </div>
+      `;
+    return prodEl;
+  }
+}
+
+// THIS CLASS IS THE LIST OF PRODUCTS
+class ProductList {
+  products = [
     new Product(
       'A Pillow',
       'https://images.unsplash.com/photo-1445991842772-097fea258e7b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
@@ -27,31 +53,24 @@ const productList = {
       'A beautiful carpet you might like',
       89.99
     )
-  ],
+  ];
+
+  constructor() {}
+
   render() {
     const renderHook = document.getElementById('app'); // Reference to place where element will be displayed on page.
     const prodList = document.createElement('ul'); // Create a list of products
     prodList.className = 'product-list'; // for styling from CSS
     for (const prod of this.products) { // to render each product
-      const prodEl = document.createElement('li'); // Create an 'li' element 
-      prodEl.className = "product-item"; // for styling from CSS
-      prodEl.innerHTML = `
-      <div>
-        <img src="${prod.imageUrl}" alt="${prod.title}" >
-        <div class="product-item__content"> 
-          <h2>${prod.title}</h2>
-          <h3>\$${prod.price}</h3>
-          <p>${prod.description}</p>
-          <button>Add To Cart</button>
-        </div>
-      </div>
-      `;
+      const productItem = new ProductItem(prod);
+      const prodEl = productItem.render();
       prodList.append(prodEl);
       console.log('prodEl - ', prodEl);
     }
     renderHook.append(prodList);
     console.log('prodList -', prodList);
   }
-};
+}
 
+const productList = new ProductList();
 productList.render();
